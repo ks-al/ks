a75d8ca221782fd63167ba3d64a24575c7cc9aec
ks-al <ksgoutami56@gmail.com>
2025-06-20 07:23:32 +0530

    Add files via upload
    Added all project files for level2 for task1

--- script.js
+++ script.js
@@ -1,32 +1,17 @@
-function convertTemperature() {
-  const tempInput = document.getElementById("temperature").value;
-  const unit = document.getElementById("unit").value;
-  const resultDiv = document.getElementById("result");
+let display = document.getElementById("display");
 
-  if (tempInput === "") {
-    resultDiv.innerHTML = "❗ Please enter a value.";
-    return;
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
   }
-
-  const temp = parseFloat(tempInput);
-  let celsius, fahrenheit, kelvin;
-
-  if (unit === "celsius") {
-    celsius = temp;
-    fahrenheit = (temp * 9/5) + 32;
-    kelvin = temp + 273.15;
-  } else if (unit === "fahrenheit") {
-    celsius = (temp - 32) * 5/9;
-    fahrenheit = temp;
-    kelvin = celsius + 273.15;
-  } else if (unit === "kelvin") {
-    celsius = temp - 273.15;
-    fahrenheit = (celsius * 9/5) + 32;
-    kelvin = temp;
-  }
-
-  resultDiv.innerHTML = 
-    `<p>🌡️ Celsius: ${celsius.toFixed(2)} °C</p>
-    <p>🔥 Fahrenheit: ${fahrenheit.toFixed(2)} °F</p>
-    <p>🧊 Kelvin: ${kelvin.toFixed(2)} K</p>`;
 }
